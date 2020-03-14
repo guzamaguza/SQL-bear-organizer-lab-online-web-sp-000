c27a68425015229009b130bcdbd9429d10249b2e
Mark A Guza <guzamaguza@gmail.com>
2020-03-14 20:58:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Mean", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy",1, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy",1, "Male", "Black", "Mean", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha",3, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha",3, "Female", "Brown", "Nice", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa",4, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa",4, "Female", "Black", "Playful", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch",4, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch",4, "Male", "White", "Mean", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy",2, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy",2, "Female", "White", "Playful", 1);
 
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",9, "Male", "Brown", "Nice", 1);
 
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",2, "Male", "Brown", "Nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "Male", "Black", "Angry", 1);
