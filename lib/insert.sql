69768fc014f61378d23f84aca85082b0998db649
Mark A Guza <guzamaguza@gmail.com>
2020-03-14 21:24:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "Male", "Brown", "Mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",1, "M", "Black", "Mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",1, "M", "Black", "goofy", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",3, "F", "Brown", "Nice", 1);
 
@@ -12,4 +12,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy"
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",9, "M", "Brown", "Nice", 0);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "M", "Black", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "M", "Black", "goofy", 0);
