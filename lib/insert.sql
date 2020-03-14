eb0ef6bc6c42333ebb704592a9a052435c5516fa
Mark A Guza <guzamaguza@gmail.com>
2020-03-14 20:47:56 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
+
+INSERT INTO cats (name, age, breed, net_worth) VALUES ("Maru", 3, "Scottish Fold", 1000000);
